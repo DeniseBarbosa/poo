4f6aac83c656449dd6554455b890401e250d303c
DeniseBarbosa <deniseluvoir@gmail.com>
2022-08-26 14:50:30 -0300

    Trabalhando com classes e instância de objetos

---
+++ src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[]args){
+        System.out.println("Aprendendo POO com Java");
+
+        // instanciando uma classe e construindo um objeto
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.nome = "Denise";
+        pessoa1.idade = 21;
+
+        System.out.println(pessoa1.nome);
+        System.out.println(pessoa1.idade);
+
+        Carro carro = new Carro();
+        carro.ano = 2022;
+        carro.cor = "azul";
+        carro.modelo = "Jeep";
+
+        System.out.println(carro.ano);
+        System.out.println(carro.cor);
+        System.out.println(carro.modelo);
+
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,12 @@
+
+public class Pessoa {
+    public String nome;
+    public Integer idade;
+}
+/** METODOS ACESSORES:
+ * PUBLIC: Indica que qualquer um pode acessar o elemento.
+ * PRIVATE: indica que somente a classe consegue acessar.
+ * PROTECTED: indica somente a classe e seus filhos consegue acessar.
+ * DEFAULT: Indica que somente a classe, seus filhos e as classes no mesmo pacote podem acessar
+ * */
+
